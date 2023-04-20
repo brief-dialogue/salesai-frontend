958d3f1ded1b3f86eeca031f1612441b2473ee33
tejasvb <tvborkar1234@gmail.com>
2023-04-21 00:27:13 +0530

    fixed build error

--- src/pages/courses/index.jsx
+++ src/pages/courses/index.jsx
@@ -1,33 +1,35 @@
 
 
 
+import ListOfChapter from '@/components/courses/listOfChapters/listOfChapter';
 import Home from '../../components/courses/home/home'
 import ListCourses from '../../components/courses/listCourses/listCourses'
 import useFetch from "../../hooks/usefetch";
 import { MDBSpinner } from "mdb-react-ui-kit";
 import { useState } from 'react';
+import { ListOfCourseFilterUrl, ListOfCourseUrl } from '../api/urls';
 
 
 export default function Courses({ loginStatus, setLoginStatus, user, setUser }) {
-  if (!user['isLogIn']) {
-    return (<p className="mx-auto text-center"> Please Log In</p>);
-  }
+
   const [query, setQuery] = useState('');
   // const [allData,setAllData] = useState(false);
   var response;
-  if (query.length == 0) {
 
-    //if(allData){
-    response = useFetch("GET", "http://localhost:1337/api/courses?&populate[thumbnail][fields][0]=url", user['token']);
-    console.log(response);
-    // }
-    // else{
-    //   response= useFetch("GET","http://localhost:1337/api/courses?pagination[start]=0&pagination[limit]=4",user['token']);
-    // }
-  } else {
-    response = useFetch("GET", `http://localhost:1337/api/courses?filters[title][$contains]=${query.trim()}`, user['token']);
+
+  //if(allData){
+  response = useFetch("GET",
+    query.trim().length == 0 ? ListOfCourseUrl() :
+      ListOfCourseFilterUrl(query)
+    , user['token']);
+  // }
+  // else{
+  //   response= useFetch("GET","http://localhost:1337/api/courses?pagination[start]=0&pagination[limit]=4",user['token']);
+  // }  
+
+  if (!user['isLogIn']) {
+    return (<p className="mx-auto text-center"> Please Log In</p>);
   }
-
   return (
     <>
       <Home onSubmitHandler={(query) => setQuery(query)} />
